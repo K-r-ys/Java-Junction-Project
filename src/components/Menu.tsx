f78751ba22550389b0f11a669952e5fbc67fb5ee
Christopher <ndiranguchristopher81@gmail.com>
2025-01-06 23:06:18 +0300

    Added coffee panels

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,42 +15,83 @@ const Menu: React.FC = () => {
   const [message, setMessage] = useState<string | null>(null);
   const navigate = useNavigate();
 
-  const menuItems: MenuItem[] = [
-    {
-      id: 1,
-      name: "Latte",
-      price: 4.5,
-      image: "/Images/Latte.jpeg",
-    },
-    {
-      id: 2,
-      name: "Cappuccino",
-      price: 4.0,
-      image: "/Images/Cappuccino.jpeg",
-    },
+  // Default Coffee Menu
+  const espressotMenu: MenuItem[] = [
+    { id: 1, name: "Latte", price: 4.5, image: "/Images/Latte.jpeg" },
+    { id: 2, name: "Cappuccino", price: 4.0, image: "/Images/Cappuccino.jpeg" },
     {
       id: 3,
-      name: "Iced Coffee",
+      name: "Mocha",
       price: 3.5,
-      image: "/Images/Iced coffee.jpeg",
+      image: "/Images/Mocha.jpeg",
+    },
+    { id: 4, name: "Macchiato", price: 5.0, image: "/Images/Macchiato.jpeg" },
+    { id: 5, name: "Flat White", price: 4.7, image: "/Images/Flat white.jpeg" },
+    { id: 6, name: "Espresso", price: 3.0, image: "/Images/Espresso.jpeg" },
+  ];
+
+  // Cold Coffee Menu
+  const coldCoffeeMenu: MenuItem[] = [
+    { id: 7, name: "Cold Brew", price: 4.5, image: "/Images/Cold brew.jpeg" },
+    { id: 8, name: "Frappe", price: 5.0, image: "/Images/Frappe.jpeg" },
+    { id: 9, name: "Iced Latte", price: 4.8, image: "/Images/Iced latte.jpeg" },
+    {
+      id: 10,
+      name: "Nitro Coffee",
+      price: 5.5,
+      image: "/Images/Nitro.jpeg",
     },
     {
-      id: 4,
-      name: "Macchiato",
+      id: 11,
+      name: "Iced Caramel Macchiato ",
+      price: 5.5,
+      image: "/Images/Iced caramel macchiato.jpeg",
+    },
+    {
+      id: 12,
+      name: "Iced Mocha ",
+      price: 5.5,
+      image: "/Images/Iced mocha.jpeg",
+    },
+  ];
+
+  // Regional Coffee Menu
+  const regionalCoffeeMenu: MenuItem[] = [
+    {
+      id: 13,
+      name: "Turkish Coffee",
+      price: 4.0,
+      image: "/Images/Turkish coffee.jpeg",
+    },
+    {
+      id: 14,
+      name: "Ethiopian Coffee",
       price: 5.0,
-      image: "/Images/Macchiato.jpeg",
+      image: "/Images/Ethiopian coffee.jpeg",
     },
     {
-      id: 5,
-      name: "Flat White",
-      price: 4.7,
-      image: "/Images/Flat white.jpeg",
+      id: 15,
+      name: "Italian Affogato",
+      price: 4.5,
+      image: "/Images/Italian affogato.jpeg",
     },
     {
-      id: 6,
-      name: "Espresso",
-      price: 3.0,
-      image: "/Images/Espresso.jpeg",
+      id: 16,
+      name: "Vietnamese Egg Coffee",
+      price: 5.2,
+      image: "/Images/Vietnamese egg coffee.jpeg",
+    },
+    {
+      id: 17,
+      name: "Kopi Luwak (Indonesia)",
+      price: 5.2,
+      image: "/Images/Kopi luwak.jpeg",
+    },
+    {
+      id: 18,
+      name: "Cafe De Olla (Mexico)",
+      price: 5.2,
+      image: "/Images/Cafe de olla.jpeg",
     },
   ];
 
@@ -69,18 +110,52 @@ const Menu: React.FC = () => {
 
   return (
     <div className="menu-container">
-      {menuItems.map((item) => (
-        <div
-          key={item.id}
-          className="coffee-card"
-          onClick={() => handleOrder(item)}
-        >
-          <img src={item.image} alt={item.name} className="coffee-image" />
-          <div className="coffee-name">{item.name}</div>
-          <div className="coffee-price">${item.price.toFixed(2)}</div>
+      <div className="menu-panels">
+        {/* Espresso Menu */}
+        <div className="menu-panel">
+          {espressotMenu.map((item) => (
+            <div
+              key={item.id}
+              className="coffee-card"
+              onClick={() => handleOrder(item)}
+            >
+              <img src={item.image} alt={item.name} className="coffee-image" />
+              <div className="coffee-name">{item.name}</div>
+              <div className="coffee-price">${item.price.toFixed(2)}</div>
+            </div>
+          ))}
         </div>
-      ))}
 
+        {/* Cold Coffee Menu */}
+        <div className="menu-panel">
+          {coldCoffeeMenu.map((item) => (
+            <div
+              key={item.id}
+              className="coffee-card"
+              onClick={() => handleOrder(item)}
+            >
+              <img src={item.image} alt={item.name} className="coffee-image" />
+              <div className="coffee-name">{item.name}</div>
+              <div className="coffee-price">${item.price.toFixed(2)}</div>
+            </div>
+          ))}
+        </div>
+
+        {/* Regional Coffee Menu */}
+        <div className="menu-panel">
+          {regionalCoffeeMenu.map((item) => (
+            <div
+              key={item.id}
+              className="coffee-card"
+              onClick={() => handleOrder(item)}
+            >
+              <img src={item.image} alt={item.name} className="coffee-image" />
+              <div className="coffee-name">{item.name}</div>
+              <div className="coffee-price">${item.price.toFixed(2)}</div>
+            </div>
+          ))}
+        </div>
+      </div>
       {message && (
         <div className={`order-message ${message ? "visible" : ""}`}>
           {message}
